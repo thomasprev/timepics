08c6b196d866c56f9019f738a98ecaeb3e061df7
Thomas <prevost.thomas@gmail.com>
2013-05-25 11:52:11 +0200

    ruby 01

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timepics::Application.config.secret_token = 'dd792b8b81bf7a8b6fbe547a9aeb44f27515810b0f42bbe684018adfae3602fb1afc514cee34331b334c580e90d080dee23a557e0ca9dafa5174615c0772ac77'
