1180aecf7047b318112ed7251e8fe556b072f6f4
Fabien <fsaulier@gmail.com>
2013-05-25 20:47:37 +0200

    Display pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -4,4 +4,11 @@ class Picture
   field :title, type: String
   field :date, type: DateTime
   field :description, type: String
+  
+  
+  def image_path
+     "/data_test/#{file}"
+  end
+  
+  
 end
